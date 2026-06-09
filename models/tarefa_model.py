687abffa8f8e91e5ce6a0cb5e659ea52b01be794
Daniel Gonçalves <danielbrgoncalves@gmail.com>
2026-06-09 17:50:04 -0300

    feature: add models and extensions for tarefa and usuario

---
+++ models/tarefa_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, Integer, Boolean, ForeignKey
+from extensions import Base
+
+
+class Tarefa(Base):
+    __tablename__ = "tarefa"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_usuario: Mapped[int] = mapped_column(ForeignKey("usuario.id"), nullable=False)
+    titulo: Mapped[str] = mapped_column(String(150), nullable=False)
+    descricao: Mapped[str] = mapped_column(String(150), nullable=False)
+    concluida: Mapped[bool] = mapped_column(Boolean, nullable=False)
---
+++ models/usuario_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, Integer
+from extensions import Base
+
+
+class Usuario(Base):
+    __tablename__ = "usuario"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(150), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False)
+    senha_hash: Mapped[int] = mapped_column(String(300), nullable=False)
+    
